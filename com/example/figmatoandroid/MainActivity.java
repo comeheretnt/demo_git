a9e69f7d7ae53bb98655bc428a89ffdc7dd85d67
comeheretnt <minh.pham220208@vnuk.edu.vn>
2024-03-12 11:21:41 +0700

    first commit

---
+++ com/example/figmatoandroid/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.figmatoandroid;
+
+
+
+/*
+ *	This content is generated from the API File Info.
+ *	(Alt+Shift+Ctrl+I).
+ *
+ *	@desc
+ *	@file 		android_small___1
+ *	@date 		Tuesday 05th of March 2024 12:36:13 AM
+ *	@title 		Page 1
+ *	@author
+ *	@keywords
+ *	@generator 	Export Kit v1.3.figma
+ *
+ */
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+
+    private View _bg__android_small___1_ek2;
+    private View rectangle_1;
+    private View rectangle_2;
+    private TextView tk_;
+    private TextView mk_;
+    private View rectangle_3;
+    private View rectangle_4;
+    private TextView log_in;
+    private TextView sig_in;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.android_small___1);
+
+
+        _bg__android_small___1_ek2 = (View) findViewById(R.id._bg__android_small___1_ek2);
+        rectangle_1 = (View) findViewById(R.id.rectangle_1);
+        rectangle_2 = (View) findViewById(R.id.rectangle_2);
+        tk_ = (TextView) findViewById(R.id.tk_);
+        mk_ = (TextView) findViewById(R.id.mk_);
+        rectangle_3 = (View) findViewById(R.id.rectangle_3);
+        rectangle_4 = (View) findViewById(R.id.rectangle_4);
+        log_in = (TextView) findViewById(R.id.log_in);
+        sig_in = (TextView) findViewById(R.id.sig_in);
+
+
+        //custom code goes here
+
+    }
+}
+
